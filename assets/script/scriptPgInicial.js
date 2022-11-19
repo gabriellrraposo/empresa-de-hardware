f14f771070f642ca54e78a8558353f51cefea1ea
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-11-19 19:51:04 -0300

    Modifica dinamicamente a logo no header e no footer dependendo do tema habilitado

--- assets/script/scriptPgInicial.js
+++ assets/script/scriptPgInicial.js
@@ -1,8 +1,38 @@
 let linkCss = document.getElementById('linkCss')
-    let mudaTema = () => {
-        
-        let href = linkCss.getAttribute("href")
-        href = href == "./assets/style/styleDark.css" ? linkCss.setAttribute("href", "./assets/style/style.css") : linkCss.setAttribute("href", "./assets/style/styleDark.css")
+let logo = document.getElementsByClassName('logo')
+
+
+let mudaTema = () => {
+    let tema = linkCss.getAttribute('tema')
+
+    let href = linkCss.getAttribute("href")
     
+    if (href == "./assets/style/styleDark.css") {
+        linkCss.setAttribute("tema", "claro")
+        tema = 'claro'
+    } else if ("./assets/style/style.css") {
+        linkCss.setAttribute("tema", "escuro")
+        tema = 'escuro'
+    }
+    
+    if (tema == "claro") {
+        linkCss.setAttribute('href', "./assets/style/style.css")
+        logo[0].setAttribute("src", "./assets/imagens/Logo/logoHorizontal.png")
+        logo[0].setAttribute("width", "180px")
+
+        logo[1].setAttribute("src", "./assets/imagens/Logo/logoVertical.png")
+        logo[1].setAttribute("width", "120px")
+        
+    } else if (tema == "escuro") {
+        linkCss.setAttribute('href', "./assets/style/styleDark.css")
+        
+        logo[0].setAttribute("src", "./assets/imagens/Logo/logoHorizontalClara.png")
+        logo[0].setAttribute("width", "180px")
+
+        logo[1].setAttribute("src", "./assets/imagens/Logo/logoVerticalClara.png")
+        logo[1].setAttribute("width", "120px")
+    }
 }
+
+
 
