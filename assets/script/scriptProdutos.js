c137b2fd9716eadc331840acf40fdf588fd86306
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-11-23 10:28:07 -0300

    Faz o botão ficar ativado enquanto a página dele está ativa

--- assets/script/scriptProdutos.js
+++ assets/script/scriptProdutos.js
@@ -1,12 +1,29 @@
 let Gpu = document.getElementById("placa-de-video")
 let Cpu = document.getElementById("processador")
+let btnGpu = document.getElementById("btnGpu")
+let btnCpu = document.getElementById("btnCpu")
 
 let mostraGpu = () => {
     Gpu.style.display = "grid"
+    btnGpu.style.backgroundColor = "var(--roxo)"
+    btnGpu.style.color = "var(--backgroundPrimario)"
+    
     Cpu.style.display = "none"
+    btnCpu.style.backgroundColor = "transparent"
+    btnCpu.style.color = "var(--corCaracteres)"
 }
 
 let mostraCpu = () => {
     Cpu.style.display = "grid"
+    btnCpu.style.backgroundColor = "var(--roxo)"
+    btnCpu.style.color = "var(--backgroundPrimario)"
+
     Gpu.style.display = "none"
+    btnGpu.style.backgroundColor = "transparent"
+    btnGpu.style.color = "var(--corCaracteres)"
+}
+
+let hoverBotoes = (element) => {
+    element.style.backgroundColor = "var(--roxo)"
+    element.style.color = "var(--backgroundPrimario)"
 }
